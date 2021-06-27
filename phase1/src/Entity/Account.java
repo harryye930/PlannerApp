b85d0054d4f491d47641da579bac40da298598a9
Charlie <66709630+charlie06728@users.noreply.github.com>
2021-06-27 19:12:39 +0800

    Update Account.java and InterfaceInfo.java

--- phase1/src/Entity/Account.java
+++ phase1/src/Entity/Account.java
@@ -1,13 +1,99 @@
 package Entity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 /**
  * A instance of this class represent a account of this application.
  */
-public class Account {
-    private boolean is_User;
-    private String user_id;
+public class Account implements InterfaceInfo{
+    private boolean isAdmin;
+    private String userName;
+    private String userId;
+    private String password;
+    private String[] header;
+    private ArrayList<String[]> data = new ArrayList<>();
+
+    Account() {
+        this.isAdmin = false;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsAdmin() {
+        return this.isAdmin;
+    }
+
+    public void read_csv(String file_path) {
+        File file = new File(file_path);
+        try {
+            if (file.createNewFile()) {
+                // The file hasn't already be created before this execution.
+                // Initialize the csv file.
+                String[] head = {"object", "kind", "widget", "info", "account", "stage"};
+                this.header = head;
+                FileWriter csvFile = new FileWriter(file);
+                int i = 0;
+                for (; i < head.length - 1; i++){
+                    csvFile.append(head[i]).append(",");
+                }
+                csvFile.append(head[i]).append("\n");
+                csvFile.flush();
+                csvFile.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Read in the data from csv file.
+        try {
+            this.read_in_data(file_path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void read_in_data(String file_path) throws FileNotFoundException {
+        // Read in the data from csv file.
+        Scanner scanner = new Scanner(new FileInputStream(file_path));
+
+        // read in the header.
+        if (scanner.hasNext()) {
+            String header_info = scanner.nextLine();
+            this.header = header_info.split(",");
+        }
+
+        // read in the data.
+        String row;
+        while (scanner.hasNext()){
+            row = scanner.nextLine();
+            String[] curr = row.split(",");
+            //ArrayList<String> arr = new A
+            this.data.add(row.split(","));
+        }
+        scanner.close();
+    }
 
 }
+
