46643de110c50e52755ce7ab721c440161d74ea5
jess-zhai <jessica.zhai@mail.utoronto.ca>
2021-06-27 19:56:13 +0800

    Edited AccountManager file

--- phase1/src/UseCase/AccountManager.java
+++ phase1/src/UseCase/AccountManager.java
@@ -1,4 +1,21 @@
 package UseCase;
 
-public class AccountManager {
+import Entity.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AccountManager implements Serializable {
+    private HashMap<String, Account> idToAccount = new HashMap<>();
+
+    private void createRegAcc(){
+
+    }
+    private void createAdminAcc(){
+
+    }
+    public void createAccount(){
+
+    }
 }
