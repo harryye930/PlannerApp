b7f75d2bbb956936b3acea75d51632e86372f977
harry <harry.ye@mail.utoronto.ca>
2021-06-29 07:34:02 +0800

    [Harry's commit] creating Planner, TaskPlanner, ChronologyPlanner

--- phase1/src/Entity/Planner.java
+++ phase1/src/Entity/Planner.java
@@ -1,4 +1,32 @@
 package Entity;
+/** the planner entity
+ * @author Runlong, Zifan
+ * @version 1.0
+ */
 
-public class Planner {
+public abstract class Planner {
+
+    /** Show the current planner
+     * @return a string represent this planner's content
+     */
+    public abstract String Display();
+
+
+    /** add agenda to current planner
+     * @return true iff the agenda is correctly added to current planner
+     */
+    public abstract Boolean Add();
+
+    /** edit agenda to current planner
+     * @return true iff the agenda is correctly edited on current planner
+     */
+    public abstract Boolean Edit();
+
+    /** delete agenda to current planner
+     * @return true iff the agenda is correctly deleted from current planner
+     */
+    public abstract boolean Delete();
+
+
+
 }
---
+++ phase1/src/Entity/TaskPlanner.java
@@ -0,0 +1,43 @@
+package Entity;
+
+public class TaskPlanner extends Planner {
+    /**
+     * Show the current planner
+     *
+     * @return a string represent this planner's content
+     */
+    @Override
+    public String Display() {
+        return null;
+    }
+
+    /**
+     * add agenda to current planner
+     *
+     * @return true iff the agenda is correctly added to current planner
+     */
+    @Override
+    public Boolean Add() {
+        return null;
+    }
+
+    /**
+     * edit agenda to current planner
+     *
+     * @return true iff the agenda is correctly edited on current planner
+     */
+    @Override
+    public Boolean Edit() {
+        return null;
+    }
+
+    /**
+     * delete agenda to current planner
+     *
+     * @return true iff the agenda is correctly deleted from current planner
+     */
+    @Override
+    public boolean Delete() {
+        return false;
+    }
+}
