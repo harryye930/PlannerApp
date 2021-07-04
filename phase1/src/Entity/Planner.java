7deeae64a781507301c472622b4dae7c61f021dc
harry <harry.ye@mail.utoronto.ca>
2021-07-04 17:22:51 +0800

    update Planner and add task completion functionality (i.e. "write csc207 entity completed".

--- phase1/src/Entity/Planner.java
+++ phase1/src/Entity/Planner.java
@@ -1,7 +1,5 @@
 package Entity;
 
-import java.sql.Time;
-
 /** the planner entity
  * @author Runlong, Zifan
  * @version 1.0
@@ -10,29 +8,31 @@ import java.sql.Time;
 public abstract class Planner {
 
 
-    /** show the current planner
+    /** Show the current planner
+     *
      * @return a string represent this planner's content
      */
     public abstract String toString();
-//    public abstract Object Display();
 
 
-    /** add agenda to current planner
+    /** Add agenda to current planner
+     *
      * @param s agenda of the agenda user wish to add
      * @return true iff the agenda is correctly added to current planner
      */
     public abstract Boolean Add(String s);
 
 
-
-    /** edit agenda to current planner
+    /** Edit agenda to current planner
+     *
      * @param i index of the agenda user wish to edit
      * @param s content of the agenda user wish to edit
      * @return true iff the agenda is correctly edited on current planner
      */
     public abstract Boolean Edit(int i, String s);
 
-    /** delete agenda to current planner
+    /** Delete agenda to current planner
+     *
      * @param i index of the agenda user wish to delete
      * @return true iff the agenda is correctly deleted from current planner
      */
