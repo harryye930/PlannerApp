e23c1bdd8fe3e142c2a226c83b5c5293f4bf29ed
jinyoung <jiny.lee@mail.utoronto.ca>
2021-07-12 03:38:10 -0400

    Added methods to Template entities, updated TemplateManager key change

--- phase1/src/Entity/DailyTemplate.java
+++ phase1/src/Entity/DailyTemplate.java
@@ -1,5 +1,7 @@
 package Entity;
 
+import java.util.Arrays;
+
 /**
  * Represents one type of Template in the program - a Daily Template, which allows users to create a Planner where
  * users can fill in tasks planned for hours in a particular day.
@@ -26,6 +28,17 @@ public class DailyTemplate extends Template{
 //        stringRep += "Increment: " + this.increment + System.lineSeparator();
 //        return stringRep;
 //    }
+
+    /**
+     * Indexes of of specific setting required for all DailyTemplates (i.e., start time, end time, increment), and
+     * an index indicating where the set of prompts asked for every events starts.
+     */
+    private final int START_TIME = 0;
+    private final int END_TIME = 1;
+    private final int INCREMENT = 2;
+
+    private final int PROMPTS_START = 3;
+
     public DailyTemplate(String name, String[] prompts) {
         super(name, prompts);
     }
@@ -35,5 +48,32 @@ public class DailyTemplate extends Template{
         return "Daily";
     }
 
-    // TODO: override get prompts
+    /**
+     * @return the setting prompt for start time
+     */
+    public String startTimePrompt() {
+        return this.getPrompts()[START_TIME];
+    }
+
+    /**
+     * @return the setting prompt for end time
+     */
+    public String endTimePrompt() {
+        return this.getPrompts()[END_TIME];
+    }
+
+    /**
+     * @return the setting prompt for time increment
+     */
+    public String incrementPrompt() {
+        return this.getPrompts()[INCREMENT];
+    }
+
+    /**
+     * @return the prompts for event detail (i.e., set of prompts asked for each event)
+     */
+    public String[] eventPrompts() {
+        String[] holder = this.getPrompts();
+        return Arrays.copyOfRange(holder, PROMPTS_START, holder.length);
+    }
 }
