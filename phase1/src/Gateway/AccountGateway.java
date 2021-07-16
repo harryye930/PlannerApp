4c89a7bf907ac2f88deab176caaff89b53126ed0
Charlie <66709630+charlie06728@users.noreply.github.com>
2021-07-16 14:40:33 +0800

    Simplify AccountGateway.

--- phase1/src/Gateway/AccountGateway.java
+++ phase1/src/Gateway/AccountGateway.java
@@ -15,7 +15,6 @@ import java.util.List;
 public class AccountGateway extends Reader {
 
     //Assign the file path of data.
-    private final String filePath = "phase1/src/UserData/";
     private String idMapPath = "phase1/src/UserData/idMap.ser";
     private String emailMapPath = "phase1/src/UserData/emailMap.ser";
 
@@ -37,7 +36,7 @@ public class AccountGateway extends Reader {
      * @return A boolean value representing whether the loading process is successful or not.
      */
     public boolean load() {
-        return this.readHashMaps() && this.readAccounts();
+        return this.readMaps();
     }
 
     /**
@@ -46,11 +45,11 @@ public class AccountGateway extends Reader {
      * @return A boolean value representing whether the loading process is successful or not.
      */
     public boolean save() {
-        return this.writeAccounts(am) && this.writeMaps();
+        return this.writeMaps();
     }
 
     // Private methods.
-    private boolean readHashMaps() {
+    private boolean readMaps() {
         try {
             this.idToAccount = (HashMap<String, Account>) super.readSer(this.idMapPath);
             am.setIdToAccount(this.idToAccount);
@@ -65,72 +64,8 @@ public class AccountGateway extends Reader {
         }
     }
 
-    private boolean readAccounts() {
-        try {
-            for (String id : this.idToAccount.keySet()){
-                Account acc = (Account) super.readSer(idMapPath + id + ".ser");
-                this.idToAccount.put(id, acc);
-                this.emailToAccount.put(acc.getEmail(), acc);
-            }
-            return true;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return false;
-        }
-    }
-
-    private boolean writeAccounts(AccountManager am) {
-        boolean fg = true;
-        ArrayList<Account> allAccount = am.getAllAccount();
-        for (Account acc : allAccount) {
-            if (!super.writeSer(filePath + acc.getUserId() + ".ser", acc)) {
-                fg = false;
-            }
-            this.emailToAccount.put(acc.getUserId(), acc);
-            this.idToAccount.put(acc.getEmail(), acc);
-        }
-        return fg;
-    }
-
-    private boolean writeAccount (Account acc){
-        String id = acc.getUserId();
-        String thisPath = this.filePath + id + ".ser";
-
-        try {
-            File nf = new File(thisPath);
-            if (nf.createNewFile() || nf.delete()) {
-                FileOutputStream fileOut = new FileOutputStream(thisPath);
-                ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                objectOut.writeObject(acc);
-                objectOut.close();
-                return true;
-            }
-            return false;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return false;
-        }
-    }
-
     private boolean writeMaps() {
-        return this.writeMap(this.emailToAccount, this.emailMapPath) && (this.writeMap(idToAccount, this.idMapPath));
+        return super.writeSer(this.idMapPath, this.idToAccount) &&
+                super.writeSer(this.emailMapPath, this.emailToAccount);
     }
-
-    private boolean writeMap(HashMap<String, Account> hm, String fp) {
-        try {
-            File nf = new File(fp);
-            if (nf.createNewFile() || nf.delete()) {
-                FileOutputStream fileOut = new FileOutputStream(fp);
-                ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                objectOut.writeObject(hm);
-                objectOut.close();
-                return true;
-            }
-            return false;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return false;
-        }
-    }
-
 }
