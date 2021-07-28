6eef752e2b8b60a3ef97895f2a5e1509eed9f14a
Charlie <charlie.ni@mail.utoronto.ca>
2021-07-29 00:02:37 +0800

    Add README.txt
    Delete unused class

--- phase1/src/Exceptions/WrongAccTypeException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class WrongAccTypeException extends java.lang.Exception {
-    public WrongAccTypeException(){
-        System.out.println("Wring class type");
-    }
-}
