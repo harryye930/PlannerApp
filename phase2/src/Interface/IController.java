b723a0e443302defe007b3a5c8a33593ec2ff171
Xiaoxue Yang <xiaoxue.yang@mail.utoronto.ca>
2021-08-02 18:27:18 -0400

    Updated javadoc for IController.java (grammar updates, removed extra lines, etc.)

--- phase2/src/Interface/IController.java
+++ phase2/src/Interface/IController.java
@@ -7,10 +7,9 @@ public interface IController {
      * Login the user with given retriever and password.
      * @param retriever A string representing the User ID or Email.
      * @param password A String representing the password.
-     * @return A boolean value representing whether the login has success or not.
+     * @return A boolean value representing whether the login is successful or not.
      */
     public boolean logIn(String retriever, String password);
-
 
     /**
      * Return the account Role.
@@ -19,7 +18,6 @@ public interface IController {
      */
     public String accountRole(String retriever);
 
-
     /**
      * Return the account Role.
      * @return A String indicating the account role, "admin", "regular" or "trial".
@@ -27,32 +25,29 @@ public interface IController {
     public String accountRole();
 
     /**
-     * Return the ID of currently logged in user, null if user did not login
+     * Return the ID of the user currently logged in, null if there is no user currently logged in.
      * @return A String representing the user ID.
      */
     public String getCurrentId();
 
     /**
      * Return a String of planners owned by current user.
-     * @return A String representing the information of planners.
+     * @return A String representing the information about planners.
      */
     public String viewUserPlanners();
-
 
     /**
      * Return a String of planners owned by current user.
      * @param retriever A String representing user id or email.
-     * @return A String representing the information of planners.
+     * @return A String representing the information about planners.
      */
     public String viewUserPlanners(String retriever);
 
-
     /**
      * Return a String of public planners.
-     * @return A String representing the information of public planners.
+     * @return A String representing the information about public planners.
      */
     public String viewPublicPlanners();
-
 
     /**
      * Create an account.
@@ -63,13 +58,11 @@ public interface IController {
      */
     public String createAccount(String email, String userName, String password);
 
-
     /**
      * Create a Daily Planner.
      * @return A String representing the id of the created planner.
      */
     public String createDailyPlanner();
-
 
     /**
      * Create a project Planner.
@@ -77,13 +70,11 @@ public interface IController {
      */
     public String createProjectPlanner();
 
-
     /**
      * View the templates' information.
-     * @return A String representing the information of the data.
+     * @return A String representing the information about the templates.
      */
     public String viewTemplates();
-
 
     /**
      * Check the template, similar to login so that the controller will
@@ -94,39 +85,32 @@ public interface IController {
      */
     public boolean checkTemplate(String id);
 
-
     /**
-     * View the information of specific planner.
+     * View information about a specific planner.
      * @param id A String representing the id of the planner we want to check.
-     * @return A String representing the information about the planner
+     * @return A String representing the information about the planner.
      */
     public String viewPlanner(String id);
 
-
     /**
-     * View the information of specific planner.
-     * @return A String representing the information about the planner with
+     * View information about a specific planner.
+     * @return A String representing the information about the planner with the
      * current planner id.
      */
     public String viewPlanner();
 
-
     /**
-     *
      * @return A boolean value representing the status of the planner, true if it
      * is private, false if it is public.
      */
     public boolean getPlannerStatus();
 
-
     /**
-     *
      * @param id A String representing the planner id we want to check.
      * @return A boolean value representing the status of the planner, true if it
      * is private, false if it is public.
      */
     public boolean getPlannerStatus(String id);
-
 
     /**
      * Change the planner status without parameter, from private to public or public
@@ -134,83 +118,72 @@ public interface IController {
      */
     public void setPlannerStatus();
 
-
     /**
      * Delete the planner with given id.
-     * @param id A String representing the id of the planner we want to
+     * @param id A String representing the id of the planner we want to delete.
      * @return A boolean value representing whether the deletion is successful or not.
      */
     public boolean deletePlanner(String id);
 
-
     /**
      * Check the planner with given id, similar to the login process.
-     * @param id A String representing the if the planner we want to check.
+     * @param id A String representing the ID of the planner we want to check.
      * @return A boolean value representing whether the planner is available to the
      * user or not.
      */
     public boolean checkPlanner(String id);
 
-
     /**
      * Edit the planner with given time and agenda
-     * @param time A String representing the timeslot or index of the planner. For daily planner,
-     *             a timeslot in form of HH:MM is needed. for project planner, a integer number is
-     *             needed.
+     * @param time A String representing the timeslot or index of the planner.
+     *             For daily planner, a timeslot in the form of HH:MM is needed.
+     *             For project planner, an integer is needed.
      * @param agenda A String representing the agenda you want to assign.
      * @return A boolean value representing whether the edit is successful or not.
      */
     public boolean editDailyPlanner(String  time, String agenda);
 
-
     /**
-     * Get the planner type, Daily planner or Project Planner
+     * Get the planner type, Daily planner or Project Planner.
      * @return A String representing the planner type.
      */
     public  String getPlannerType();
 
-
     /**
-     * Save the data in ser. files.
+     * Save the data in .ser files.
      */
     public void saveProgram();
 
-
     /**
-     * Delete the account with current retriever.
+     * Delete the account with the current retriever.
      */
     public void deleteAccount();
-
 
     /**
      * Change the password of the current retriever.
      * @param original A String representing the original password.
      * @param newPassword A String representing the new password.
-     * @return A boolean value representing whether the reset is successful or not.
+     * @return A boolean value representing whether the update is successful or not.
      */
     public boolean changePassword(String original, String newPassword);
 
-
     /**
-     * Get the account information.
+     * Get account information.
      * @return A String representing the account information.
      */
     public String getAccountInfo();
 
-
     /**
-     * Change the username with given new name.
-     * @param newName A String representing the new name want to assign.
+     * Change the username to newName.
+     * @param newName A String representing the new name we want to assign.
      */
     public void changeUserName(String newName);
 
-
     /**
-     * Return a collection of the planner ids that owned by the current user.
-     * @return A ArrayList representing planner ids.
+     * Return a collection of IDs of the planners that are owned by the current user.
+     * @return An ArrayList representing planner ids.
      */
     public ArrayList<String> getPlannerIds();
-
 
     /**
      * Return all the user information in a collection.
