682a4ef2089356a5d338019599e394cc6e5f6eac
Charlie <charlie.ni@mail.utoronto.ca>
2021-08-02 16:33:44 +0800

    Add java doc to the Widget.java

--- phase2/src/UserInterface/Widgets/Widget.java
+++ phase2/src/UserInterface/Widgets/Widget.java
@@ -4,24 +4,48 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
 
+
+/**
+ * A Class represent a fundamental element of UI.
+ */
 public class Widget {
     protected Widget parent = null;
     protected Widget child;
     protected String text;
 
+
+    /**
+     *
+     * @return Return the information or prompt about this Widget.
+     */
     @Override
     public String toString() {
         return this.text;
     }
 
+
+    /**
+     *
+     * @param parent A widget to be set as the parent of current widget.
+     */
     public void setParent(Widget parent) {
         this.parent = parent;
     }
 
+
+    /**
+     *
+     * @param child A widget to be set as the child of the current widget.
+     */
     public void setChild(Widget child) {
         this.child = child;
     }
 
+
+    /**
+     * call the trigger method of the the child widget since in some cases
+     *          the widget wight hold.
+     */
     public void unhold() {
         if (this.child == null) {
             System.out.println("Child widget is not initialized.");
@@ -30,6 +54,10 @@ public class Widget {
         }
     }
 
+
+    /**
+     * Trigger the widget.
+     */
     public void trigger() {
         throw new NotImplementedException();
     }
