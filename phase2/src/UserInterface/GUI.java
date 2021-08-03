af4ff84ea11b54865ce9b0d69672c1e6a8c66f0f
harry <harry.ye@mail.utoronto.ca>
2021-08-03 12:11:01 +0800

    update GUI login page, will learn how to create a new page

--- phase2/src/UserInterface/GUI.java
+++ phase2/src/UserInterface/GUI.java
@@ -8,62 +8,62 @@ import java.util.Objects;
 public class GUI implements ActionListener {
 
     private JFrame frame = new JFrame();
+    JPanel page;
+    JPanel loginPage;
+
     JLabel usernameLabel;
     JLabel passwordLabel;
     JLabel loginStatus;
+
     JTextField usernameText;
     JTextField passwordText;
+
     JButton loginButton;
     JButton newAccountButton;
     JButton guestButton;
 
 
+
     public GUI() {
-        frame.setSize(350, 200);
         loginPage();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Login Page");
         frame.pack();
         frame.setVisible(true);
     }
+    private void newPage(){
 
+
+        JPanel page = new JPanel();
+        frame.add(page, BorderLayout.CENTER);
+        page.setLayout(null);
+        JLabel test = new JLabel("New page!");
+        page.add(test);
+    }
 
     private void loginPage(){
-        JPanel loginPage = new JPanel();
+        loginPage = new JPanel(new GridLayout(4,2));
         frame.add(loginPage, BorderLayout.CENTER);
-        loginPage.setLayout(null);
 
         usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(10,20,80,25);
         loginPage.add(usernameLabel);
-
         usernameText = new JTextField(20);
-        usernameText.setBounds(100,20,165,25);
         loginPage.add(usernameText);
 
         passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10,50,80,25);
         loginPage.add(passwordLabel);
-
         passwordText = new JPasswordField(20);
-        passwordText.setBounds(100,50,165,25);
         loginPage.add(passwordText);
 
         loginButton = new JButton("Login");
         loginButton.addActionListener(this);
-        loginButton.setBounds(40, 100, 160, 50);
         loginPage.add(loginButton);
-
         newAccountButton = new JButton("Create account");
-        newAccountButton.setBounds(40, 160, 160, 50);
         loginPage.add(newAccountButton);
-
         guestButton = new JButton("Continue as guest");
-        guestButton.setBounds(40, 220, 160, 50);
         loginPage.add(guestButton);
 
         loginStatus = new JLabel("");
-        loginStatus.setBounds(10, 80, 300, 25);
         loginPage.add(loginStatus);
 
 
