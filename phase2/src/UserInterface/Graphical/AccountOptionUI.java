5dc52e3849ed3e4582d3ad202b8295df8b95de69
Charlie <charlie.ni@mail.utoronto.ca>
2021-08-06 01:51:02 +0800

    Implement AccountOptionUI

--- phase2/src/UserInterface/Graphical/AccountOptionUI.java
+++ phase2/src/UserInterface/Graphical/AccountOptionUI.java
@@ -9,16 +9,25 @@ import java.awt.event.ActionEvent;
 public class AccountOptionUI extends GeneralPresenter {
     private boolean flag = false;
 
-    //JPanel
-    JPanel accountMenu = new JPanel();
+    //Panel
+    private final JPanel accountMenu = new JPanel();
+    private JPanel changeMenu;
+    JScrollPane accounts;
 
-    //Buttons
-    private final JButton changeUserEmailButton = new JButton("Change Your Associated Email");
-    private final JButton changeUserPasswordButton = new JButton("Change Your Password");
-    private final JButton deleteAccountButton = new JButton("Delete your account");
-    private final JButton returnToMainButton = new JButton("Return to Main Menu");
+    //Button
+    private final JButton changName = new JButton("Change User name");
+    private final JButton changePassword = new JButton("Change Password");
+    private final JButton back = new JButton("Go back");
+    private JButton submit0;
+    private JButton submit1;
+    private JButton goBack;
 
+    //Text
+    private JTextField text0;
+    private JPasswordField text1;
 
+    //Label
+    private JLabel message;
 
     public AccountOptionUI(String parent) {
         this.setParent(parent);
@@ -30,6 +39,7 @@ public class AccountOptionUI extends GeneralPresenter {
     @Override
     public void run() {
         if (this.flag) {
+            accounts = data.getAccounts();
             cl.show(main, "accountMenu");
         } else {
             this.showAccountMenu();
@@ -42,23 +52,82 @@ public class AccountOptionUI extends GeneralPresenter {
         accountMenu.setLayout(null);
         main.add(accountMenu, "accountMenu");
 
-        JScrollPane accounts = data.getAccounts();
+        accounts = data.getAccounts();
         accounts.setBounds(75, 25, 450, 100);
         accountMenu.add(accounts);
 
+        changName.setBounds(515, 150, 150, 40);
+        changePassword.setBounds(515, 200, 150, 40);
+        back.setBounds(515, 250, 70, 40);
+        accountMenu.add(changName);
+        accountMenu.add(changePassword);
+        accountMenu.add(back);
+        changName.addActionListener(this);
+        changePassword.addActionListener(this);
+        back.addActionListener(this);
+    }
 
-        accountMenu.add(changeUserEmailButton);
-        accountMenu.add(changeUserPasswordButton);
-        accountMenu.add(deleteAccountButton);
-        accountMenu.add(returnToMainButton);
+    private void changeName() {
+        changeMenu = new JPanel();
+        changeMenu.setLayout(null);
+        main.add(changeMenu, "changeMenu");
 
-        changeUserEmailButton.addActionListener(this);
-        changeUserPasswordButton.addActionListener(this);
-        deleteAccountButton.addActionListener(this);
-        returnToMainButton.addActionListener(this);
+        message = new JLabel("Please enter your new user name:");
+        message.setBounds(125, 100, 500, 50);
+        message.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        changeMenu.add(message);
 
+        text0 = new JTextField();
+        text0.setBounds(125, 175, 400, 40);
+        changeMenu.add(text0);
 
+        submit0 = new JButton("Submit");
+        submit0.setBounds(275, 275, 100, 40);
+        submit0.addActionListener(this);
+        changeMenu.add(submit0);
 
+        goBack = new JButton("Go back");
+        goBack.setBounds(275, 325, 100, 40);
+        goBack.addActionListener(this);
+        changeMenu.add(goBack);
+
+        cl.show(main, "changeMenu");
+    }
+
+    private void changePassword() {
+        changeMenu = new JPanel();
+        changeMenu.setLayout(null);
+        main.add(changeMenu, "changeMenu");
+
+        message = new JLabel("Please enter your original password:");
+        message.setBounds(125, 50, 500, 50);
+        message.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        changeMenu.add(message);
+
+        text0 = new JTextField();
+        text0.setBounds(125, 100, 400, 40);
+        changeMenu.add(text0);
+
+        JLabel message0 = new JLabel("Please enter your new password:");
+        message0.setBounds(125, 150, 500, 50);
+        message0.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        changeMenu.add(message0);
+
+        text1 = new JPasswordField();
+        text1.setBounds(125, 200, 400, 40);
+        changeMenu.add(text1);
+
+        submit1 = new JButton("Submit");
+        submit1.setBounds(275, 300, 100, 40);
+        submit1.addActionListener(this);
+        changeMenu.add(submit1);
+
+        goBack = new JButton("Go back");
+        goBack.setBounds(275, 350, 100, 40);
+        goBack.addActionListener(this);
+        changeMenu.add(goBack);
+
+        cl.show(main, "changeMenu");
     }
 
     /**
@@ -68,18 +137,28 @@ public class AccountOptionUI extends GeneralPresenter {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == changeUserEmailButton){
-            System.out.println("remove after implement");
-        }
-        else if (e.getSource() == changeUserPasswordButton){
-            System.out.println("remove after implement");
-        }
-        else if (e.getSource() == returnToMainButton){
-            System.out.println("remove after implement");
-        }
-        else if (e.getSource() == returnToMainButton){
+        if (e.getSource() == back) {
             cl.show(main, this.getParent());
+        } else if (e.getSource() == changName) {
+            this.changeName();
+        } else if (e.getSource() == changePassword) {
+            this.changePassword();
+        } else if (e.getSource() == submit0) {
+            controller.changeUserName(text0.getText());
+            main.remove(changeMenu);
+            this.run();
+            //cl.show(main, "accountMenu");
+        } else if (e.getSource() == submit1) {
+            if (controller.changePassword(text0.getText(), text1.getText())) {
+                main.remove(changeMenu);
+                this.run();
+                //cl.show(main, "accountMenu");
+            } else {
+                text0.setText("Invalid input, please try again!");
+            }
+        } else if (e.getSource() == goBack) {
+            cl.show(main, "accountMenu");
+            main.remove(changeMenu);
         }
-
     }
 }
