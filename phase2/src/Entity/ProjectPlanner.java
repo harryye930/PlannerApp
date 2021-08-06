05f25e940015ef2a55f45f60cd233fc0c51ee148
harry <harry.ye@mail.utoronto.ca>
2021-08-06 18:05:46 +0800

    set up new ProjectPlanner accroding to template structure

--- phase2/src/Entity/ProjectPlanner.java
+++ phase2/src/Entity/ProjectPlanner.java
@@ -1,182 +1,225 @@
 package Entity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ProjectPlanner extends Planner {
-    private ArrayList<String> tasks;
-    private ArrayList<String> taskStatus;
-    private int ID;
-    private int NumAgendas;
+    String plannerName;
+    String firstColName;
+    String secondColName;
+    String thirdColName;
+    ArrayList<String> allProjectEvents;
+    ArrayList<String> firstColEvents;
+    ArrayList<String> secondColEvents;
+    ArrayList<String> thirdColEvents;
+
+    public ProjectPlanner(String plannerName, String firstColName, String secondColName, String thirdColName) {
+        this.plannerName = plannerName;
+        this.firstColName = firstColName;
+        this.secondColName = secondColName;
+        this.thirdColName = thirdColName;
+
+        this.firstColEvents = new ArrayList<String>();
+        this.secondColEvents = new ArrayList<String>();
+        this.thirdColEvents = new ArrayList<String>();
+        this.allProjectEvents = new ArrayList<String>();
 
 
-    /**
-     * initialize ProjectPlanner
-     * @param plannerName: the name of the planner
-     */
-    public ProjectPlanner(String plannerName) {
-        super(plannerName);
-        this.tasks = new ArrayList<>();
-        this.taskStatus = new ArrayList<>();
-        this.ID = super.getID();
-        this.NumAgendas = 0;
     }
 
 
-    /** Show the planner type is project planner
+    /**
+     * Show the current planner
      *
-     * @return a string represent this planner is project planner
+     * @return a string represent this planner's content
      */
-    public String getType(){
+    @Override
+    public String toString() {
+        String allEvent ="All Project Planner events:" + this.allProjectEvents.toString() + "\n";
+        String firstColumnEvents = "All events in "+ this.firstColName+ ": " + this.firstColEvents.toString() + "\n";
+        String secondColumnEvents = "All events in "+ this.secondColName+ ": " + this.secondColEvents.toString() + "\n";
+        String thirdColumnEvents = "All events in "+ this.thirdColName+ ": " + this.thirdColEvents.toString() + "\n";
+        String plannerName = this.plannerName + "\n";
+        return plannerName +
+                allEvent +
+                firstColumnEvents +
+                secondColumnEvents +
+                thirdColumnEvents;
+
+
+    }
+
+    /**
+     * @return a String representing of the planner type.
+     */
+    @Override
+    public String getType() {
         return "project";
     }
 
-
-    /** Show the current planner
+    /**
+     * Show the number of planner agendas.
      *
-     * @return a string representation of this planner's content
+     * @return a int representing of the number of planner agendas.
      */
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("Status").append(this.privacyStatus).append("\n");
-        String plannerInfo = this.plannerName + "\n" + "ID: " + ID + "\n" + "\nTasks: \n";
-        sb.append(plannerInfo);
-        if (this.tasks.isEmpty()){
-            sb.append("N/A");
-        }
-        for (int i = 0; i < this.tasks.size(); i++){
-            sb.append(this.tasks.get(i));
-            sb.append("--");
-            sb.append(this.taskStatus.get(i));
-            sb.append("\n");
-        }
-        return sb.toString();
+    @Override
+    public int getNumAgendas() {
+        return this.allProjectEvents.size();
     }
 
-
-    /** Add agenda to current planner
+    /**
+     * Add agenda to current planner -- default to first column
      *
-     * @param agenda the agenda user wish to add
+     * @param s agenda of the agenda user wish to add
      * @return true iff the agenda is correctly added to current planner
      */
     @Override
-    public Boolean add(String agenda){
-        if (this.tasks.contains(agenda)){
-            return false;
-        }
-        else if (agenda.length() == 0){
-            return false;
-        }
-        else{
-            this.tasks.add(agenda);
-            this.taskStatus.add("not completed");
-            this.NumAgendas ++;
-            return true;
-        }
+    public Boolean add(String s) {
+        return this.add(s, this.firstColName);
     }
 
-
-    /** Add agenda to current planner - index specified
-     *
-     * @param i index of new agenda item
-     * @param agenda content of new item
-     * @return true iff the agenda is correctly added
-     */
-    public Boolean add(int i, String agenda) {
-        if (i > this.tasks.size() - 1){ // if i is over the size limit
-            i = this.tasks.size() - 1;
-            this.tasks.add(i, agenda);
-            this.taskStatus.add(i, "not completed");
-            this.NumAgendas ++;
-            return true;
-        }
-        else{
-            this.tasks.add(i, agenda);
-            this.taskStatus.add(i, "not completed");
-            this.NumAgendas ++;
-            return true;
-        }
-    }
-
-
-    /** Complete certain agenda item
-     *
-     * @param Agenda: the agenda item to change status to completed
-     * @return true iff the agenda is successfully changed to complete
-     */
-    public Boolean complete(String Agenda){
-        if (this.tasks.contains(Agenda)){
-            int index = this.tasks.indexOf(Agenda);
-            if (this.taskStatus.get(index).equals("not completed")){
-                this.taskStatus.set(index, "completed");
+    public Boolean add(String s, String columnName) {
+        if (Objects.equals(this.firstColName, columnName)) {
+            if (!this.allProjectEvents.contains(s)) {
+                this.firstColEvents.add(s);
+                this.allProjectEvents.add(s);
                 return true;
+            } else {
+                return false;
             }
-
+        } else if (Objects.equals(this.secondColName, columnName)) {
+            if (!this.allProjectEvents.contains(s)) {
+                this.secondColEvents.add(s);
+                this.allProjectEvents.add(s);
+                return true;
+            } else {
+                return false;
+            }
+        } else if (Objects.equals(this.thirdColName, columnName)) {
+            if (!this.allProjectEvents.contains(s)) {
+                this.thirdColEvents.add(s);
+                this.allProjectEvents.add(s);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
-        return false;
     }
 
-
-    /** Edit agenda to current planner
+    /**
+     * Edit agenda to current planner
      *
-     * @param i index of the agenda user wish to edit
-     * @param agenda content of the agenda user wish to edit
+     * @param i        index of the agenda user wish to edit
+     * @param newEvent content of the agenda user wish to edit
      * @return true iff the agenda is correctly edited on current planner
      */
     @Override
-    public Boolean edit(int i, String agenda) {
-        if (agenda.length() == 0) {  // if the new agenda is empty
-            return false;
-        } else if (tasks.size() == 0){
-            return add(agenda);
-        } else if (i > this.tasks.size() - 1) { // if i is over the size limit
-            return this.add(i,agenda);
-        }else{
-            this.tasks.set(i, agenda);
-            this.taskStatus.set(i, "not completed");
+    public Boolean edit(int i, String newEvent) {
+        if (i < this.allProjectEvents.size() && !this.allProjectEvents.contains(newEvent)) {
+            String oldAgenda = this.allProjectEvents.get(i);
+            this.allProjectEvents.set(i, newEvent);
+            if (this.firstColEvents.contains(oldAgenda)) {
+                this.firstColEvents.set(this.firstColEvents.indexOf(oldAgenda), newEvent);
+            } else if (this.secondColEvents.contains(oldAgenda)) {
+                this.secondColEvents.set(this.secondColEvents.indexOf(oldAgenda), newEvent);
+            } else if (this.thirdColEvents.contains(oldAgenda)) {
+                this.thirdColEvents.set(this.thirdColEvents.indexOf(oldAgenda), newEvent);
+            }
             return true;
-        }
-    }
-
-
-    /** Delete agenda item on current planner
-     *
-     *
-     * @return true iff the agenda is correctly deleted from current planner
-     */
-    public boolean delete(int i) {
-        if (i > this.tasks.size() - 1){ // if i is over the size limit
+        } else {
             return false;
         }
-        else{
-            this.tasks.remove(i);
-            this.taskStatus.remove(i);
-            return true;
-        }
     }
-
 
     /**
-     * delete the agenda from the task
-     * @param agenda the String representation of the agenda
-     * @return true if successfully removed agenda from task; false if otherwise.
+     * edit agenda item by providing existing event name and new event name
+     *
+     * @param oldEvent name of old event name
+     * @param newEvent name of new event name
+     * @return true iff an event is been changed.
      */
-    public boolean delete(String agenda) {
-        if (!this.tasks.contains(agenda)){ // if agenda is not one of the task
+    public Boolean edit(String oldEvent, String newEvent) {
+        if (this.allProjectEvents.contains(oldEvent)) {
+            int index = this.allProjectEvents.indexOf(oldEvent);
+            return this.edit(index, newEvent);
+        } else {
             return false;
-        }
-        else{
-            this.taskStatus.remove(this.tasks.indexOf(agenda));
-            this.tasks.remove(agenda);
-            return true;
         }
     }
 
+    public Boolean editColumn(String eventName, String newColumnName) {
+        if (this.getColumnName(eventName) != "ERROR"){  // the event exist and is in one of the column
+           String oldColumnName = this.getColumnName(eventName);
+           if (oldColumnName != newColumnName){ // the desire column is different tha current event column
+               if (this.validColumn(newColumnName)){  // if the new column is valid
+                   ArrayList<String> oldColumnEvents = this.getColumnEvent(oldColumnName);
+                   ArrayList<String> newColumnEvents = this.getColumnEvent(newColumnName);
+                   oldColumnEvents.remove(eventName);
+                   newColumnEvents.add(eventName);
+                   return true;
+               }
+               else{
+                   return false;
+               }
+           }
+           else{
+               return false;
+           }
+        }
+        else{
+            return false;
+        }
+    }
 
-    /**
-     * get the number of agendas of this planner
-     * @return int of number of agendas.
-     */
-    public int getNumAgendas(){
-        return this.NumAgendas;
+    public Boolean delete(String eventName) {
+        if (!this.getColumnName(eventName).equals("ERROR")) { //means event exist and we can have the col name
+            String columnName = this.getColumnName(eventName);
+            ArrayList<String> columnEvents = this.getColumnEvent(columnName);
+            columnEvents.remove(eventName);
+            this.allProjectEvents.remove(eventName);
+            return true;
+
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private String getColumnName(String eventName) {
+        if (this.allProjectEvents.contains(eventName)) {
+            if (this.firstColEvents.contains(eventName)) {
+                return this.firstColName;
+            } else if (this.secondColEvents.contains(eventName)) {
+                return this.secondColName;
+
+            } else if (this.thirdColEvents.contains(eventName)) {
+                return this.thirdColName;
+            } else {
+                return "ERROR";
+            }
+
+        } else {
+            return "ERROR";
+        }
+    }
+
+    private ArrayList<String> getColumnEvent(String colName) {
+        if (colName.equals(this.firstColName)) {
+            return this.firstColEvents;
+        } else if (colName.equals(this.secondColName)) {
+            return this.secondColEvents;
+        } else {  // if (colName.equals(this.thirdColName))
+            return this.thirdColEvents;
+        }
+    }
+
+    private Boolean validColumn(String columnName){
+        return (columnName == this.firstColName ||
+                columnName == this.secondColName ||
+                columnName == this.thirdColName);
     }
 }
+
