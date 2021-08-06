99c57d61ecea0645eff59de43f25ebe9b5afd262
jinyoung <jiny.lee@mail.utoronto.ca>
2021-08-05 23:55:32 -0400

    Updated AccountManager - Created a method (with helper methods) that can randomly generate either good or weak password.

--- phase2/src/UseCase/AccountManager.java
+++ phase2/src/UseCase/AccountManager.java
@@ -6,8 +6,7 @@ import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -121,6 +120,248 @@ public class AccountManager implements Serializable{
         } else { // All the criteria are met.
             return "Good";
         }
+    }
+
+    /**
+     * Returns a random number between min and max, inclusive.
+     * @param min A lowest number that can be generated.
+     * @param max A highest number that can be generated.
+     * @return An integer that is generated at random between min and max, both inclusive.
+     */
+    private int randomNumberGenerator(int min, int max) {
+        Random randomNumberGenerator = new Random();
+        return randomNumberGenerator.nextInt(max - min + 1) + min;
+    }
+
+    /**
+     * Returns a random upper case or lower case letter.
+     * @param isUpper A boolean indicating whether it returns an upper case letter or lower case letter.
+     * @return A String that's a random upper case or lower case letter.
+     */
+    private String randomAlphabet(boolean isUpper) {
+        int letterASCII; // ASCII code
+        int randomAlphabet = randomNumberGenerator(1, 26);
+        if (isUpper) {
+            // ASCII code for A-Z is 65 to 90, inclusive
+            letterASCII = randomAlphabet + 64;
+        } else {
+            // ASCII code for a-z is 97 to 122, inclusive
+            letterASCII = randomAlphabet + 96;
+        }
+        return Character.toString((char)letterASCII);
+    }
+
+    /**
+     * Returns a random integer between 0-9 (inclusive).
+     * @return A String that's a random integer between 0-9 (inclusive).
+     */
+    private String randomNumber() {
+        int randomNumber = randomNumberGenerator(0, 9);
+        return Integer.toString(randomNumber);
+    }
+
+    /**
+     * Returns a random special characters from the following:
+     */
+    private String randomSpecial() {
+        String[] randomCharacters = {"!", "@", "#", "$", "%", "&"};
+        int randomIndex = randomNumberGenerator(0, 5);
+        return randomCharacters[randomIndex];
+    }
+
+    /**
+     * Returns random String from the following options:
+     * (1) An upper case
+     * (2) A lower case
+     * (3) An integer between 0 and 9, inclusive
+     * (4) A special character (i.e., !, @, #, $, %, or &)
+     * @return A String that's a randomly generated an upper case, lower case, integer or special character.
+     */
+    private String generateRandomCharacter() {
+        String randomString;
+        int randomCriterion = randomNumberGenerator(1, 4); // Random number between 1 - 4
+        if (randomCriterion == 1) {
+            randomString = randomAlphabet(true); // Upper case
+        } else if (randomCriterion == 2) {
+            randomString = randomAlphabet(false); // Lower case
+        } else if (randomCriterion == 3) {
+            randomString = randomNumber();
+        } else {
+            randomString = randomSpecial();
+        }
+        return randomString;
+    }
+
+    /**
+     * Returns true if it meets a given criterion.
+     * Precondition: Criterion must be between 2-4; otherwise, returns false
+     * @param s A string that is being checked.
+     * @param criterionNumber A criterion it is checking for.
+     * @return A boolean which indicates whether the provided string matches the given criterion or not.
+     */
+    public boolean matchesCriteria(String s, int criterionNumber) {
+        String atLeastOneUpper = ".*[A-Z].*";
+        String atLeastOneLower = ".*[a-z].*";
+        String atLeastOneNumber = ".*[0-9].*";
+
+        if (criterionNumber == 2) {
+            return Pattern.matches(atLeastOneUpper, s);
+        } else if (criterionNumber == 3) {
+            return Pattern.matches(atLeastOneLower, s);
+        } else if (criterionNumber == 4) {
+            return Pattern.matches(atLeastOneNumber, s);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns a good random password that fits following password criteria.
+     * Password criteria include:
+     * (1) at least 4 characters long,
+     * (2) must include at least one upper case letter,
+     * (3) must include at least one lower case letter, and
+     * (4) must include at least one number.
+     */
+    public String goodRandomPasswordGenerator() {
+        StringBuilder randomPassword = new StringBuilder();
+        ArrayList<String> temporaryRandomPassword = new ArrayList<>();
+
+        // First, randomly select password length between 4 and 8 and fulfill the remaining criterion.
+        int passwordLength = randomNumberGenerator(4, 8);  // Criteria 1 - length
+        temporaryRandomPassword.add(randomAlphabet(true));  // Criteria 2 - upper case
+        temporaryRandomPassword.add(randomAlphabet(false)); // Criteria 3 - lower case
+        temporaryRandomPassword.add(randomNumber());  // Criteria 4 - number
+        // We know that all criteria for the good password is met. Now generate random upper character, lower character,
+        // number, or a special character to meet the pre-generated random length of the password.
+        String randomString;
+        for (int i = 3; i < passwordLength; i++) {
+            // Randomly select from either upper or lower character, number, or special character.
+            randomString = generateRandomCharacter();
+            temporaryRandomPassword.add(randomString);
+        }
+        // Randomly shuffle the order
+        Collections.shuffle((temporaryRandomPassword));
+
+        // Finally, build the randomPassword
+        for (String s: temporaryRandomPassword) {
+            randomPassword.append(s);
+        }
+        return randomPassword.toString();
+    }
+
+    /**
+     * Returns a weak random password that meets 3 of the following criteria.
+     * Password criteria include:
+     * (1) at least 4 characters long,
+     * (2) must include at least one upper case letter,
+     * (3) must include at least one lower case letter, and
+     * (4) must include at least one number.
+     * @return weakPassword A String that's a weak password.
+     */
+    public String weakRandomPasswordGenerator() {
+        StringBuilder randomPassword = new StringBuilder();
+        ArrayList<String> temporaryRandomPassword = new ArrayList<>();
+        int passwordLength;
+
+        int randomCriterionToBeNotMet = randomNumberGenerator(1, 4);
+        if (randomCriterionToBeNotMet == 1) {
+            passwordLength = 3;
+            temporaryRandomPassword.add(randomAlphabet(true));
+            temporaryRandomPassword.add(randomAlphabet(false));
+            temporaryRandomPassword.add(randomNumber());
+        } else {
+            passwordLength = randomNumberGenerator(4, 8);
+            if (randomCriterionToBeNotMet != 2) {
+                temporaryRandomPassword.add(randomAlphabet(true));
+            }
+            if (randomCriterionToBeNotMet != 3) {
+                temporaryRandomPassword.add(randomAlphabet(false));
+            }
+            if (randomCriterionToBeNotMet != 4) {
+                temporaryRandomPassword.add(randomNumber());
+            }
+        }
+        // We know that all randomly selected criteria are met. Now generate random upper character, lower character,
+        // number, or a special character to meet the pre-generated random length of the password.
+        String randomString;
+        int currentLength = temporaryRandomPassword.size();
+        for (int i = currentLength; i < passwordLength; i++) {
+            // Randomly select from either upper or lower character, number, or special character.
+            do {
+                randomString = generateRandomCharacter();
+            } while (matchesCriteria(randomString, randomCriterionToBeNotMet));
+            temporaryRandomPassword.add(randomString);
+        }
+        // Randomly shuffle the order
+        Collections.shuffle((temporaryRandomPassword));
+
+        // Finally, build the randomPassword
+        for (String s: temporaryRandomPassword) {
+            randomPassword.append(s);
+        }
+        return randomPassword.toString();
+    }
+
+    /**
+     * Returns either a good or weak random password. A password is determined good or weak based on the following
+     * password criteria:
+     * (1) at least 4 characters long,
+     * (2) must include at least one upper case letter,
+     * (3) must include at least one lower case letter, and
+     * (4) must include at least one number.
+     * @param isGood A boolean indicating whether a randomly generated password should be good or weak.
+     * @return A String that's a randomly generated password that is either good or weak.
+     */
+    public String randomPasswordGenerator(boolean isGood) {
+        StringBuilder randomPassword = new StringBuilder();
+        ArrayList<String> temporaryRandomPassword = new ArrayList<>();
+        int passwordLength, randomCriterionToBeNotMet;
+
+        // First, if the password is weak then a criterion that should not be meet is selected at random; otherwise,
+        // all criteria should be met.
+        if (isGood) {
+            randomCriterionToBeNotMet = 0;  // Non-existing criterion (i.e., all criteria should be met)
+        } else {
+            randomCriterionToBeNotMet = randomNumberGenerator(1, 4);
+        }
+        // Meet the necessary password criteria.
+        if (randomCriterionToBeNotMet == 1) {
+            passwordLength = 3;
+            temporaryRandomPassword.add(randomAlphabet(true));
+            temporaryRandomPassword.add(randomAlphabet(false));
+            temporaryRandomPassword.add(randomNumber());
+        } else {
+            passwordLength = randomNumberGenerator(4, 8);
+            if (randomCriterionToBeNotMet != 2) {
+                temporaryRandomPassword.add(randomAlphabet(true));
+            }
+            if (randomCriterionToBeNotMet != 3) {
+                temporaryRandomPassword.add(randomAlphabet(false));
+            }
+            if (randomCriterionToBeNotMet != 4) {
+                temporaryRandomPassword.add(randomNumber());
+            }
+        }
+        // We know that all required criteria are met. Now generate random upper character, lower character,
+        // number, or a special character to meet the pre-generated random length of the password.
+        String randomString;
+        int currentLength = temporaryRandomPassword.size();
+        for (int i = currentLength; i < passwordLength; i++) {
+            // Randomly select from either upper or lower character, number, or special character.
+            do {
+                randomString = generateRandomCharacter();
+            } while (matchesCriteria(randomString, randomCriterionToBeNotMet));
+            temporaryRandomPassword.add(randomString);
+        }
+        // Randomly shuffle the order
+        Collections.shuffle((temporaryRandomPassword));
+
+        // Finally, build the randomPassword
+        for (String s: temporaryRandomPassword) {
+            randomPassword.append(s);
+        }
+        return randomPassword.toString();
     }
 
     /**
