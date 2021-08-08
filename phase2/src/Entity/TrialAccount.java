f78efa50b5119e31497eba61f333156b1f758a95
hashimr1 <67606070+hashimr1@users.noreply.github.com>
2021-08-08 17:07:59 -0400

    TemporaryAccount and TrialAccount

--- phase2/src/Entity/TrialAccount.java
+++ phase2/src/Entity/TrialAccount.java
@@ -7,7 +7,7 @@ public class TrialAccount extends Account{
 
     public TrialAccount() {
         super();
-        this.accountType = "trial";
+        super.accountType = "trial";
     }
 
     /**
