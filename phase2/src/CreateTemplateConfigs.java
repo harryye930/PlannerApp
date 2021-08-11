8ce70616455c6b8493fa59e5e4358e054cb20026
Xiaoxue Yang <xiaoxue.yang@mail.utoronto.ca>
2021-08-11 01:16:20 -0400

    Added a class used for creating idToTemplate.ser config file (for testing purposes)

---
+++ phase2/src/CreateTemplateConfigs.java
@@ -0,0 +1,59 @@
+import Controller.TemplateController;
+import Entity.DailyTemplate;
+import Entity.ProjectTemplate;
+import Entity.RemindersTemplate;
+import Entity.Template;
+import Gateway.TemplateGateway;
+import UseCase.TemplateManager;
+
+/**
+ * Creates template config file.
+ */
+public class CreateTemplateConfigs {
+    public static void main(String[] args) {
+        // Create a daily template
+        String dailyTemplateName = "Default Daily Template";
+        String dailyPlannerNamePrompt = "What is the name of the planner that will be created from this Daily Template?";
+        String startTimePrompt = "What is the start time of the planner? " +
+                "Enter in the form of hh:mm in 24-hour clock (e.g., 09:00 means 9am)";
+        String endTimePrompt = "What is the end time of the planner? " +
+                "Enter in the form of hh:mm in 24-hour clock (e.g., 21:00 means 9pm)";
+        String timeIncrementPrompt = "What is the time increment of the planner? Enter a multiple of 60 " +
+                "(e.g., if it's 1 hour, enter 60; if it's 2 hours, enter 120)";
+        Template dailyTemplate = new DailyTemplate(dailyTemplateName, dailyPlannerNamePrompt, startTimePrompt,
+                endTimePrompt, timeIncrementPrompt);
+        dailyTemplate.setPublishedStatus(true);
+
+        // Create a project template
+        String projectTemplateName = "Default Project Template";
+        String projectPlannerNamePrompt = "What is the name of the planner that will be created from this Project Template?";
+        String firstStatusPrompt = "Enter the column name for the first status column of the planner (e.g., \"TODO\")";
+        String secondStatusPrompt = "Enter the column name for the second status column of the planner (e.g., \"DOING\")";
+        String thirdStatusPrompt = "Enter the column name for the third status column of the planner (e.g., \"DONE\")";
+        Template projectTemplate = new ProjectTemplate(projectTemplateName, projectPlannerNamePrompt,
+                firstStatusPrompt, secondStatusPrompt, thirdStatusPrompt);
+        projectTemplate.setPublishedStatus(true);
+
+        String remindersTemplateName = "Default Reminders Template";
+        String remindersPlannerNamePrompt = "What is the name of the planner that will be created from this Reminders Template?";
+        String taskHeadingPrompt = "Enter the column name for the column containing task names in the planner " +
+                "(e.g., \"Tasks\", \"Review Items\", \"Assignments\", etc.)";
+        String dateHeadingPrompt = "Enter the column name for the column containing deadlines/dates in the planner " +
+                "(e.g., \"Deadline\", \"Due Date\", etc.)";
+        String completionStatusHeadingPrompt = "Enter the column name for the column containing completion status in the planner " +
+                "(e.g., \"Completion Status\", \"Completed?\", etc.)";
+        Template remindersTemplate = new RemindersTemplate(remindersTemplateName, remindersPlannerNamePrompt,
+                taskHeadingPrompt, dateHeadingPrompt, completionStatusHeadingPrompt);
+        remindersTemplate.setPublishedStatus(true);
+
+        // Add templates to TemplateManager
+        TemplateManager tm = new TemplateManager();
+        tm.importTemplate(dailyTemplate);
+        tm.importTemplate(projectTemplate);
+        tm.importTemplate(remindersTemplate);
+        System.out.println(tm.detailViewAllTemplates(true));
+
+        TemplateGateway templateGateway = new TemplateGateway(tm);
+        templateGateway.save();
+    }
+}
