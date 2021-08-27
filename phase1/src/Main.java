295d5ac70de0d5fb073e041979b5be16440a5d38
harryye930 <89664586+harryye930@users.noreply.github.com>
2021-08-27 17:41:20 -0400

    Delete phase1 directory

--- phase1/src/Main.java
+++
@@ -1,8 +0,0 @@
-import controller.UserActionController;
-
-public class Main extends UserActionController {
-    public static void main(String[] args) {
-        UserActionController usa = new UserActionController();
-        usa.runProgram();
-    }
-}
